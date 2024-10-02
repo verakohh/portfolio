633a109fb864c25d2bcaaa0a6016465c7e49dd4c
verakohh <vrkwjacad@gmail.com>
2024-10-02 15:02:07 +0800

    Updated styles for work experience page and centered image

---
+++ footer.js
@@ -0,0 +1,8 @@
+window.addEventListener('DOMContentLoaded', () => {
+    fetch('footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.body.insertAdjacentHTML('beforeend', data);
+        })
+        .catch(error => console.error('Error loading footer:', error));
+});
